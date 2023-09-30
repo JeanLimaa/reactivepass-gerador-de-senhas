2cf0f7fe53b208d9001f50b30f0139038908e82e
Jean Lima <93737943+JeanLimaa@users.noreply.github.com>
2023-09-29 21:59:58 -0300

    use client profile

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -2,10 +2,6 @@
 import TableProfile from "@/components/TableShowProfile/TableProfile";
 import LayoutPrivate from "@/components/LayoutPrivate";
 
-export const metadata = {
-    title: "Senhas Armazenadas - ReactivePass"
-  };
-
 export default function Profile() {
     return (
         <LayoutPrivate>
