7a1d60bf7ffca26a7d01e1d80dd162723f9a81bc
Jean Lima <93737943+JeanLimaa@users.noreply.github.com>
2023-10-05 22:42:35 -0300

    add wrapper to link repository

--- src/components/LinkRepository.jsx
+++ src/components/LinkRepository.jsx
@@ -5,8 +5,11 @@ export default function LinkRepository() {
         <a 
             href="https://github.com/JeanLimaa/reactivepass-gerador-de-senhas" 
             target="_blank" 
-            className="flex items-center gap-2 border-2 p-1 rounded duration-custom ease-custom hover:bg-gray-500 hover:text-white">
-            <Image src="/github.svg" width={33} height={33} />
+            className="flex items-center gap-2 border-2 p-1 rounded duration-custom ease-custom hover:bg-gray-500 hover:text-white"
+            >
+                <div className="w-8">
+                <Image src="/github.svg" width={100} height={100} alt="link para o repositorio" />
+                </div>
             <h2>Repositório</h2>
         </a>
     )
